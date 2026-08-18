bf9bc42264d2eacc661946eb2a81133c22d5cbf9
Hemant Gaikwad <103812753+hemantgaikwad1@users.noreply.github.com>
2026-08-18 15:59:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
-//your JS code here. If required.
+
+let btn = document.getElementById("btn");
+let ip = document.getElementById("ip");
+let output = document.getElementById("output");
+
+btn.onclick = () => {
+  let num = Number(ip.value);
+
+  
+  new Promise((res, rej) => {
+    setTimeout(() => {
+      output.innerHTML = `Result: ${num}`;
+      res(num);
+    }, 2000);
+  })
+
+  .then((val) => {
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        let result = val * 2;
+        output.innerHTML = `Result: ${result}`;
+        res(result);
+      }, 2000);
+    });
+  })
+ 
+  .then((val) => {
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        let result = val - 3;
+        output.innerHTML = `Result: ${result}`;
+        res(result);
+      }, 1000);
+    });
+  })
+
+  .then((val) => {
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        let result = val / 2;
+        output.innerHTML = `Result: ${result}`;
+        res(result);
+      }, 1000);
+    });
+  })
+
+  .then((val) => {
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        let result = val + 10;
+        output.innerHTML = `Final Result: ${result}`;
+        res(result);
+      }, 1000);
+    });
+  });
+};
